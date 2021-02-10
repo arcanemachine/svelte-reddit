4077561fa1dadabfa0f91fa142747900c87a5d9a
Nicholas Moen <arcanemachine@tutanota.com>
2021-02-09 20:59:39 -0700

    Get rid of hamburger menu

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -75,8 +75,14 @@
            fill="currentColor" viewBox="0 0 16 16">
         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
       </svg>
+    <!-- settings icon -->
+      <svg on:click="{() => navbarItemClicked('settings')}" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-gear navbar-end-touch-icon navbar-end-touch-icon-settings" viewBox="0 0 16 16">
+          <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+            <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
+      </svg>
     </div>
-    <div on:click="{navbarToggle}"
+    <!-- hamburger menu -->
+    <!-- div on:click="{navbarToggle}"
          class="pl-2 navbar-burger"
          class:is-active="{navbarIsActive}"
          title="Navbar Menu" alt="Navbar Menu"
@@ -87,7 +93,7 @@
       <span class="burger-line" aria-hidden="true"></span>
       <span class="burger-line" aria-hidden="true"></span>
     </div>
-  </div>
+  </div-->
   <div id="navMenu"
        class="navbar-menu"
        class:is-active="{navbarIsActive}"
@@ -96,8 +102,13 @@
       <!-- navbar items, desktop-size viewports only -->
       <div class="navbar-item has-text-centered is-hidden-touch"
            class:is-dark="{$darkModeActive}"
+           on:click="{() => navbarItemClicked('subreddit-picker')}">
+        Your Subreddits
+      </div>
+      <div class="navbar-item has-text-centered is-hidden-touch"
+           class:is-dark="{$darkModeActive}"
            on:click="{() => navbarItemClicked('subreddit-search')}">
-        Go to subreddit
+        Search
       </div>
       <!-- navbar items for all viewports  -->
       <div class="navbar-item has-text-centered"
@@ -130,6 +141,11 @@
   fill: white;
 }
 
+.navbar-brand {
+  height: 4rem;
+  width: 100%; /* push the nav items to the end if hamburger icon isn't present */
+}
+
 .navbar-title {
   white-space: nowrap;
   overflow: hidden;
@@ -150,9 +166,13 @@
   cursor: pointer;
 }
 
-
 .navbar-end-touch-icon-search {
-  margin-right: 0.25rem;
+  margin-right: 1rem;
+  padding: 0.6rem;
+}
+
+.navbar-end-touch-icon-settings {
+  margin-right: 0.75rem;
   padding: 0.6rem;
 }
 
@@ -172,7 +192,9 @@
 
 .navbar-menu {
   margin-top: -2px;
+  margin-right: 1rem; /* push the nav items to the end if hamburger icon isn't present */
   padding-bottom: 0;
+  justify-content: flex-end; /* push the nav items to the end if hamburger icon isn't present */
 }
 
 .navbar-item {
