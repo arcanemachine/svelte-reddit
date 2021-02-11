f73d7df0ca6711ba6d4729120c7adc4c2eb5b606
Nicholas Moen <arcanemachine@tutanota.com>
2021-02-11 11:59:15 -0700

    Convert edit icon into button

--- src/components/SubredditPicker.svelte
+++ src/components/SubredditPicker.svelte
@@ -80,17 +80,30 @@
       <div class="subreddit-search">
         <input class="input subreddit-search-input" type="search"
                bind:value="{subreddit}"
-               placeholder="Find subreddit by name..."
+               placeholder="Go to subreddit by name..."
                on:keyup="{e => e.key === 'Enter' && submitButtonClicked()}"
                autofocus> <!-- svelte-ignore a11y-autofocus -->
         <button class="button is-info subreddit-search-button-submit"
                 bind:this="{submitButton}"
                 on:click="{submitButtonClicked}">
-          Search
+          Go
         </button>
       </div>
 
-      <div class="mt-5 is-size-4">
+      <div class="mt-4 subreddits-edit-button-container">
+        <button class="button is-warning subreddits-edit-button"
+              class:is-active="{subredditsEditToggle}"
+              on:click="{subredditsEditToggle}">
+          Edit Subreddits
+          <svg class="ml-2 bi bi-pencil action-icon-pencil"
+               xmlns="http://www.w3.org/2000/svg"
+               width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+          </svg>
+        </button>
+      </div>
+
+      <div class="mt-4 is-size-4">
         Favorites
       </div>
 
@@ -156,17 +169,6 @@
         <div class="subject-empty-text">none</div>
       {/if}
 
-      <div class="mt-6 mb-0 ml-2 cursor-url subreddits-action-icon-container"
-           on:click="{subredditsEditToggle}">
-        <span class="hover-background-gray" style="font-size: 1.1rem;">
-          Edit
-          <svg class="ml-1 bi bi-pencil action-icon-pencil"
-               xmlns="http://www.w3.org/2000/svg"
-               width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-          </svg>
-        </span>
-      </div>
     </div>
   </div>
 </div>
@@ -254,6 +256,6 @@
 
 .subject-empty-text {
   font-style: italic;
-  color: #ddd;
+  color: #aaa;
 }
 </style>
